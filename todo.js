892d3ef81cef340718a8b9035607d6d73cbd2891
Steven <codebusiness2014@gmail.com>
2018-01-12 13:40:35 -0500

    Prevent user from submitting an empty string.

--- todo.js
+++ todo.js
@@ -7,6 +7,9 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".add-todo-button").addEventListener("click", (e) => {
     let todoList = $l("#todo-item").arr[0].value;
 
+    if (todoList === "") {
+        todoList = "I will not allow this list to be empty";
+    }
     function setRandomBGColor () {
       let letters = "123456789ABCDEF";
       let color = "#";
@@ -22,5 +25,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     $l("#ul-list").append(`<li style="background: ${color}">${todoList}</li>`);
     $l("#todo-item").arr[0].value = "";
+  });
+
+  document.querySelector(".delete-todo-button").addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(e);
+
   });
 });
