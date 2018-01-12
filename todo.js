b8ea1b64e401cddc348f52e867344c7bdb0fe4fd
Steven <codebusiness2014@gmail.com>
2018-01-12 14:30:56 -0500

    Refactoring in order to make it easier to tell which li to be deleted

--- todo.js
+++ todo.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // }
 
   document.querySelector(".add-todo-button").addEventListener("click", (e) => {
+    let liLength = document.querySelectorAll('#ul-list')[0].children.length + 1;
     let todoList = $l("#todo-item").arr[0].value;
 
     if (todoList === "") {
@@ -23,13 +24,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let color = setRandomBGColor();
 
-    $l("#ul-list").append(`<li style="background: ${color}">${todoList}</li>`);
+    $l("#ul-list").append(`<li class="${liLength}" style="background: ${color}">${todoList}  <button class="delete-todo-button" type="submit" name="button">X</button></li>`);
     $l("#todo-item").arr[0].value = "";
   });
 
   document.querySelector(".delete-todo-button").addEventListener("click", (e) => {
     e.preventDefault();
-    console.log(e);
-
+    console.log(e.target.parentNode.classList.value);
   });
 });
